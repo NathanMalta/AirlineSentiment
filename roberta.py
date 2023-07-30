3d992e5af05cd50695e7046e04c54174e38d2490
Nathan Malta <n.malta@me.com>
2023-07-29 23:12:54 -0400

    added gpu, preprocessed sentiment data

--- roberta.py
+++ roberta.py
@@ -16,10 +16,14 @@ class roBERTa():
         self.tokenizer = AutoTokenizer.from_pretrained('roberta-base')
         self.model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 
+        #move to cuda if available
+        self.model.to('cuda')
+
     def classifySentiment(self, text):
         encoded_input = self.tokenizer(text, return_tensors='pt', truncation=True, max_length=512)
+        encoded_input.to('cuda')
         output = self.model(**encoded_input)
-        scores = output[0][0].detach().numpy()
+        scores = output[0][0].detach().cpu().numpy()
         sentiment_idx = np.argmax(scores)
 
         if sentiment_idx == 0:
